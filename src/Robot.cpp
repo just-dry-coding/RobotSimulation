c1758db50bd62cb2beb377fb7eaafe1dc1b0e60e
justin <justin.gruenenwald@lexxpluss.com>
2024-06-29 12:51:55 +0900

    format + default switch

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,54 +1,51 @@
 #include "Robot.h"
+#include <cassert>
 
-Robot::Robot(Pose2D const& initalPose, Grid2D const& grid) : _currentPose(initalPose), _grid(grid)
-{
-}
+Robot::Robot(Pose2D const& initalPose, Grid2D const& grid) : _currentPose(initalPose), _grid(grid) {}
 
-void Robot::turnLeft()
-{
+void Robot::turnLeft() {
 	auto& currentDirection = _currentPose.d;
-	switch (currentDirection)
-	{
-		case Direction::n:
-			currentDirection = Direction::w;
-			return;
-		case Direction::w:
-			currentDirection = Direction::s;
-			return;
-		case Direction::s:
-			currentDirection = Direction::e;
-			return;
-		case Direction::e:
-			currentDirection = Direction::n;
-			return;
+	switch (currentDirection) {
+	case Direction::n:
+		currentDirection = Direction::w;
+		return;
+	case Direction::w:
+		currentDirection = Direction::s;
+		return;
+	case Direction::s:
+		currentDirection = Direction::e;
+		return;
+	case Direction::e:
+		currentDirection = Direction::n;
+		return;
 	}
 }
 
-Pose2D getNewPose(Pose2D const& p)
-{
-	switch (p.d)
-	{
-		case Direction::n:
-			return {p.x - 1, p.y, p.d};
-		case Direction::w:
-			return {p.x, p.y - 1, p.d};
-		case Direction::s:
-			return {p.x + 1, p.y, p.d};
-		case Direction::e:
-			return {p.x, p.y + 1, p.d};
+Pose2D getNewPose(Pose2D const& p) {
+	switch (p.d) {
+	case Direction::n:
+		return { p.x - 1, p.y, p.d };
+	case Direction::w:
+		return { p.x, p.y - 1, p.d };
+	case Direction::s:
+		return { p.x + 1, p.y, p.d };
+	case Direction::e:
+		return { p.x, p.y + 1, p.d };
+	default:
+		assert(false && "Unhandled direction in getNewPose");
+		return {};
 	}
 };
 
-bool Robot::isBlocked() const{
+bool Robot::isBlocked() const {
 	auto const suggestedPose = getNewPose(_currentPose);
 	return (_grid(suggestedPose.x, suggestedPose.y) == GridCellState::b);
 }
 
 // blocked state could be cached for better performance
-void Robot::moveForward()
-{
+void Robot::moveForward() {
 	auto const suggestedPose = getNewPose(_currentPose);
 	if (_grid(suggestedPose.x, suggestedPose.y) == GridCellState::b)
 		return;
-	_currentPose=suggestedPose;
+	_currentPose = suggestedPose;
 }
